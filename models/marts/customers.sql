3ae280e327a56d84627ae5987e10f8a143cdcd58
Simo Tumelius <simo@datamie.fi>
2025-12-11 12:55:42 +0000

    Finish Lab 1

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -1,40 +1,46 @@
 with orders as (
-    select
-        id as order_id,
-        customer_id,
-        created_at as ordered_at
-    from raw.ecomm.orders_de
+   select
+       *
+   from {{ ref('orders') }}
 ), 
 
 customers as (
-    select
-        *
-    from {{ ref('stg_ecomm__customers') }}
+   select
+       customer_id,
+       first_name,
+       last_name,
+       email,
+       address,
+       phone_number
+   from {{ ref('stg_ecomm__customers') }}
 ),
 
 customer_metrics as (
-    select
-        customer_id,
-        count(*) as count_orders,
-        min(ordered_at) as first_order_at,
-        max(ordered_at) as most_recent_order_at
-    from orders
-    group by 1
-
+   select
+       customer_id,
+       count(*) as count_orders,
+       min(ordered_at) as first_order_at,
+       max(ordered_at) as most_recent_order_at,
+       avg(delivery_time_from_collection) as average_delivery_time_from_collection,
+       avg(delivery_time_from_order) as average_delivery_time_from_order
+   from orders
+   group by 1
 ),
 
 joined as (
-    select
-        customers.*,
-        coalesce(customer_metrics.count_orders,0) as count_orders,
-        customer_metrics.first_order_at,
-        customer_metrics.most_recent_order_at
-    from customers
-    left join customer_metrics on (
-        customers.customer_id = customer_metrics.customer_id
-    )
+   select
+       customers.*,
+       coalesce(customer_metrics.count_orders,0) as count_orders,
+       customer_metrics.first_order_at,
+       customer_metrics.most_recent_order_at,
+       customer_metrics.average_delivery_time_from_collection,
+       customer_metrics.average_delivery_time_from_order
+   from customers
+   left join customer_metrics on (
+       customers.customer_id = customer_metrics.customer_id
+   )
 )
 
 select
-    *
+   *
 from joined
