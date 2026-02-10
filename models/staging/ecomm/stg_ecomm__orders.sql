2c5f9c03cfee0c8ef412c9cb3991ef807a9437b5
Simo Tumelius <simo@datamie.fi>
2026-02-10 14:31:48 +0000

    Simplify stg_ecomm__orders

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -3,12 +3,14 @@ with source as (
 ),
 
 renamed as (
-    select
-        * rename (
-            id as order_id,
-            created_at as ordered_at,
-            status as order_status
-        )
+    select  
+        id as order_id,
+        total_amount,
+        currency,
+        status as order_status,
+        created_at as ordered_at,
+        customer_id,
+        _synced_at
     from source
 )
 
